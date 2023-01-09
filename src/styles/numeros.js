d7de309ececf2d7d31733374c26cc334e0c430ae
ana.karolina <ana.karolina@ablsystem.com.br>
2023-01-09 13:49:45 -0300

    feat: componente que renderizará cada numero que pertencerá a aposta

---
+++ src/styles/numeros.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	number: {
+		color: 'white',
+		width: 30,
+		height: 30,
+		backgroundColor: 'black',
+		textAlign: 'center',
+		textAlignVertical: 'center',
+		borderRadius: 50,
+	},
+	numberBox: {
+		width: 50,
+		height: 50,
+		backgroundColor: 'white',
+		borderRadius: 50,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderWidth: 3,
+		borderColor: 'black',
+		margin: 6,
+	},
+});
