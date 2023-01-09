aaffecc2bfceadf5aad03d4e3475bcda63b6cfaa
ana.karolina <ana.karolina@ablsystem.com.br>
2023-01-09 13:48:50 -0300

    feat: regra de negócio que verificará se existe repetição de números na aposta gerada e que limpará a tela caso o usuário modifique a quantia de número a ser apostados

--- src/components/geradorDeNumeros/index.js
+++ src/components/geradorDeNumeros/index.js
@@ -1,22 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
+import { Text, FlatList, SafeAreaView, View } from 'react-native';
 import Styles from '../../styles/geradorDeNumeros';
+import Numeros from '../numeros';
 
-const geradorDeNumeros = ({ numbers }) => {
+const geradorDeNumeros = ({ numbers, clearView }) => {
+	const [view, setView] = useState(false);
+
+	useEffect(() => {
+		if (clearView) {
+			setView(clearView);
+		} else {
+			setView(false);
+		}
+	}, [clearView]);
+
+	const sortnumber = () => {
+		return parseInt(Math.random() * (60 - 1) + 1);
+	};
+
+	const hasDuplicates = (array) => {
+		for (let index = 0; index < array.length; index++) {
+			for (let i = 0; i < array.length; i++) {
+				if (array[index] === array[i] && index !== i) {
+					array[i] = parseInt(sortnumber());
+				}
+			}
+		}
+
+		return array;
+	};
+
 	const createNumbers = () => {
 		let changeNumber = parseInt(numbers);
 		let numbersArray = [];
 		if (typeof changeNumber === 'number') {
 			for (let index = 0; index < numbers; index++) {
-				let createNumbers = parseInt(Math.random() * (60 - 1) + 1);
-				numbersArray.push(createNumbers);
+				numbersArray.push(sortnumber());
 			}
 		}
 
-		return numbersArray.join(',');
+		let verifyHasDuplicates = hasDuplicates(numbersArray);
+
+		return verifyHasDuplicates;
 	};
 
-	return <Text>{createNumbers()}</Text>;
+	return (
+		<>
+			{view ? (
+				<View style={Styles.numberList}></View>
+			) : (
+				<FlatList
+					data={createNumbers()}
+					renderItem={Numeros}
+					keyExtractor={(item) => item}
+					horizontal={false}
+					numColumns={5}
+					style={Styles.numberList}
+				/>
+			)}
+		</>
+	);
 };
 
 export default geradorDeNumeros;
--- src/styles/geradorDeNumeros.js
+++ src/styles/geradorDeNumeros.js
@@ -2,25 +2,8 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-	title: {
-		fontSize: 32,
-		textAlign: 'center',
-		fontWeight: '200',
-	},
-	button: {
-		width: '70%',
-		flexGrow: 1,
-		backgroundColor: '#000',
-	},
-	input: {
-		padding: 10,
-		borderBottomWidth: 1,
-		borderBottomColor: '#000000',
-		margin: 12,
-		textAlign: 'center',
-	},
-	warning: {
-		color: `#b22222`,
-		padding: 10,
+	numberList: {
+		margin: 10,
+		flex: 1,
 	},
 });
